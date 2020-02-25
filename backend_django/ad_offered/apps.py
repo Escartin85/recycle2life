d2c6961933b17013828b9ba237ef712fc163f74a
batman <batman@localhost.localdomain>
2020-02-25 00:09:49 +0000

    Setup DjangoRestFramework 3.0

---
+++ backend_django/ad_offered/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AdOfferedConfig(AppConfig):
+    name = 'ad_offered'
---
+++ backend_django/ad_wanted/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AdWantedConfig(AppConfig):
+    name = 'ad_wanted'
